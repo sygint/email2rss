1dcb48258263a15665502c4c31dfa10599f943b7
gnarmedia <gnarmedia@users.noreply.github.com>
2018-09-05 01:47:52 -0700

    :art: (js) Add ESLint for JS linting and formatting
    Adds ESLint for JS linting and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    "airbnb",
+    "prettier"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
+}
