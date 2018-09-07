be72481cf72741c06c654097ea8e74bc26c32b09
gnarmedia <gnarmedia@users.noreply.github.com>
2018-09-06 23:55:53 -0700

    :sparkles: (server) Add Express to begin setting up the server
    Adds Express to begin setting up the server to handle the api calls
    along wih the server side rendering handled by Next.

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  // support properties on classes
-  "plugins": [
-    "transform-class-properties",
-    "@babel/plugin-transform-runtime"
-  ],
-
-  "presets": [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ]
+  "presets": ["next/babel"]
 }
